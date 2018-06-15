7370aeb4ef428ee696dbbbd6cdbc9717a8590cf0
Luca Baumgartner <luca.baumgartner@stud.bbbaden.ch>
2018-06-15 07:41:15 +0200

    updated Toolbar Controller

--- src/main/java/ch/baeumli/offergenmaven/Controllers/ToolbarSend_Controller.java
+++ src/main/java/ch/baeumli/offergenmaven/Controllers/ToolbarSend_Controller.java
@@ -15,6 +15,7 @@ import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -23,13 +24,20 @@ import javafx.fxml.Initializable;
  */
 public class ToolbarSend_Controller implements Initializable {
 
+    @FXML
+    private Button btnMail;
+    @FXML
+    private Button btnPrint;
+    @FXML
+    private Button btnFile;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
 
     @FXML
-    public void btnSendMailClick(ActionEvent event) {
+    public void btnMailClick(ActionEvent event) {
         Desktop desktop = Desktop.getDesktop();
         String msg = "mailto:lbaumgartner@hotmail.de?subject=Test&body=Test%20Message";
         URI uri = URI.create(msg);
@@ -38,6 +46,14 @@ public class ToolbarSend_Controller implements Initializable {
         } catch (IOException ex) {
             Logger.getLogger(ToolbarSend_Controller.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    @FXML
+    private void btnPrintClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnFileClick(ActionEvent event) {
     }
 
     
